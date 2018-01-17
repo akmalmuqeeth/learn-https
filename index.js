e2cb06d7b56349e9cfa3659d7b758700bb9066bf
akmal <akmal@brighter.com>
2018-01-16 18:03:03 -0800

    shift http to https

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3010
 
-var http = require('http');
-
-// create a http server
-var server = http.createServer();
+var https = require('https');
+var fs = require('fs');
+var path = require('path');
+var certsPath = path.join(__dirname, 'certs', 'server');
+// create a https server
+var server = https.createServer({
+    key: fs.readFileSync(path.join(certsPath, 'my-server-private.key.pem'))
+    , cert: fs.readFileSync(path.join(certsPath, 'my-server.crt.pem'))
+    , requestCert: false
+    , rejectUnauthorized: true
+});
 
 // forward all requests to express app
 server.on('request', app);
